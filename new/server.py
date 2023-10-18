cba3c2105a25569289db6ca75b6bce2b2951ce82
Louis Liao <buzonliao@gmail.com>
2023-10-18 21:59:23 +0800

    add send email

--- new/server.py
+++ new/server.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import csv
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -20,12 +24,36 @@ def write_to_csv(data):
         csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([name, email, message])
 
+def send_email(data):
+    html = Template(Path('mail_template.html').read_text())
+
+    name = data['name']
+    sender_email = data['email']
+    message = data['message']
+
+    email = EmailMessage()
+    email['from'] = sender_email
+    email['to'] = 'buzonliao@gmail.com'
+    email['subject'] = 'New message from http://buzonxxxx.pythonanywhere.com/'
+    email.set_content(html.substitute({'name': name, 'message': message }), 'html')
+
+    with open('pw/pw.txt', mode='r') as my_file:
+        pw = my_file.read()
+    
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('buzonxxxx@gmail.com', pw)
+        smtp.send_message(email)
+        print('all good boss!')
+
 @app.route('/submit_contact_form', methods=['POST'])
 def submit_form():
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
             write_to_csv(data)
+            send_email(data)
             return 'Form submitted successfully'
         except:
             return 'Something went wrong'
